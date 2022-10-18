ced0c42a2e89f4e72f785e2301cd1cf0c0613b15
Tomas <tmercadoslp@gmail.com>
2022-10-18 00:49:26 -0300

    feat(components/EndGameMessage): add expected worn to lose case

--- components/EndGameMessage.tsx
+++ components/EndGameMessage.tsx
@@ -3,9 +3,14 @@ import { Text } from "@chakra-ui/react";
 interface EndGameMessageProps {
   win: boolean;
   lost: boolean;
+  expectedWord: string;
 }
 
-const EndGameMessage: React.FC<EndGameMessageProps> = ({ lost, win }) => {
+const EndGameMessage: React.FC<EndGameMessageProps> = ({
+  lost,
+  win,
+  expectedWord,
+}) => {
   if (win && lost) {
     throw new Error("You can't win and lost at the same time");
   }
@@ -16,7 +21,9 @@ const EndGameMessage: React.FC<EndGameMessageProps> = ({ lost, win }) => {
 
   return (
     <Text fontSize="3xl" color={win ? "green.400" : "red.400"}>
-      {win ? "You win" : "You lost"}
+      {win
+        ? "Has acertado!"
+        : `Perdiste :( La palabra era: ${expectedWord.toUpperCase()}`}
     </Text>
   );
 };
